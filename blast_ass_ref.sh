4a23078e771b34416e4e09fd550c3cbb005e5ee7
markgrivainis <grvmar006@myuct.ac.za>
2017-10-19 12:05:43 -0400

    ready to run on phoenix

---
+++ blast_ass_ref.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#$ -cwd -S /bin/bash
+source activate tipseqhunterBLAST
+# Comment when running on phoenix
+SGE_TASK_ID=32
+
+# settings for cluster
+# module load python/3.4.3
+
+#folder=${1}
+#standard=${2}
+#label=${3}
+#cd 'input/'${folder}
+#F=(${SGE_TASK_ID}.*)
+#cd ../..
+
+#python glue.py ${F} ${standard} ${label}
+
+
+samples_file=${1}
+sample=$(sed -n "${SGE_TASK_ID}p" ${samples_file})
+python blast_assembly.py ${sample}
